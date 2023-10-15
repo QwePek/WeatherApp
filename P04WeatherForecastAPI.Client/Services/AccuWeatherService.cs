d337a284d41d3a08002439041f0df4cba7108303
QwePek <filip@eweb.pl>
2023-10-15 19:03:50 +0200

    Pliczki

---
+++ P04WeatherForecastAPI.Client/Services/AccuWeatherService.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using P04WeatherForecastAPI.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04WeatherForecastAPI.Client.Services
+{
+    internal class AccuWeatherService
+    {
+        private const string base_url = "http://dataservice.accuweather.com";
+        private const string autocomplete_endpoint = "locations/v1/cities/autocomplete?apikey={0}&q={1}&language={2}";
+        private const string current_conditions_endpoint = "currentconditions/v1/{0}?apikey={1}&language={2}";
+
+        //moje
+        private const string ipAddressSearch_endpoint = "locations/v1/cities/ipaddress?apikey={0}&q={1}&language={2}";
+        private const string geoPosition_endpoint = "locations/v1/cities/geoposition/search?apikey={0}&q={1}&language={2}";
+
+        string api_key;
+        string language;
+
+        public AccuWeatherService()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<App>()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetings.json"); 
+
+            var configuration = builder.Build();
+            api_key = configuration["api_key"];
+            language = configuration["default_language"];
+        }
+
+
+        public async Task<City[]> GetLocations(string locationName)
+        {
+            string uri = base_url + "/" + string.Format(autocomplete_endpoint, api_key, locationName, language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                City[] cities = JsonConvert.DeserializeObject<City[]>(json);
+                return cities;
+            }
+        }
+
+        public async Task<Weather> GetCurrentConditions(string cityKey)
+        {
+            string uri = base_url + "/" + string.Format(current_conditions_endpoint, cityKey, api_key,language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                string json = await response.Content.ReadAsStringAsync();
+                Weather[] weathers= JsonConvert.DeserializeObject<Weather[]>(json);
+                return weathers.FirstOrDefault();
+            }
+        }
+
+        public async Task<IpAddressInfo> GetIpAddressInfo(string ipAddress)
+        {
+            string url = base_url + "/" + string.Format(ipAddressSearch_endpoint, api_key, ipAddress, language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                IpAddressInfo ipAddresses = JsonConvert.DeserializeObject<IpAddressInfo>(json);
+                return ipAddresses;
+            }
+        }
+
+        public async Task<Geoposition> GetGeopositionInfo(string geoPos)
+        {
+            string url = base_url + "/" + string.Format(geoPosition_endpoint, api_key, geoPos, language);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                Geoposition geoPosi = JsonConvert.DeserializeObject<Geoposition>(json);
+                return geoPosi;
+            }
+        }
+    }
+}
